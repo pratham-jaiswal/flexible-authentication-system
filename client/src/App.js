4b672f62bf7198f54018a7cf05e28fab2b68d4d8
pratham-jaiswal <prathamj0502@gmail.com>
2023-08-02 15:05:37 +0530

    Manage cookie sessions

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,34 @@ import Login from "./pages/login.js";
 import Register from "./pages/register.js";
 import Dashboard from "./pages/dashboard.js";
 import './index.css'
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/data');
+        const recvData = await response.json();
+        setData(recvData);
+      } catch (error) {
+        setData({ isValid: false });
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="dark">
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login isValid={data.isValid} />} />
+          <Route path="/register" element={<Register isValid={data.isValid} />} />
+          <Route path="/dashboard" element={<Dashboard isValid={data.isValid} userData={data.userData} />} />
         </Routes>
       </Router>
     </div>
