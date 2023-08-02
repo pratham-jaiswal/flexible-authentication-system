8c6fcd3d1dddd3d62ff74aa592b590e6560b902c
pratham-jaiswal <prathamj0502@gmail.com>
2023-08-02 15:05:09 +0530

    Manage cookie sessions

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { isEmailValid, isPassEightChars } from '../utils/register.utils.js';
+import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
+export default function Login({ isValid }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isValid) {
+      navigate("/dashboard");
+      return;
+    }
+  }, [isValid, navigate]);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -17,21 +27,56 @@ export default function Login() {
     return isEmailValid(email) && isPassEightChars(password);
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    if (!isLoginFormValid()) {
+      console.error('Invalid form data');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.message === 'Login successful') {
+          window.location.href = '/dashboard';
+        }
+        else{
+          alert(data.message);
+        }
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
-      <div class="center-content">
-        <div class="card">
+      <div className="center-content">
+        <div className="card">
           <h1>Login</h1>
-          <form class="form-area" action="/login" method="POST">
-            <div class="input-container">
-              <label for="email">Email</label>
-              <input type="text" placeholder="email@example.com" value={email} onChange={handleEmailChange} required />
+          <form className="form-area" onSubmit={handleLogin}>
+            <div className="input-container">
+              <label htmlFor="email">Email</label>
+              <input type="text" placeholder="email@example.com" name="email" value={email} onChange={handleEmailChange} required />
             </div>
-            <div class="input-container">
-              <label for="password">Password</label>
-              <input type="password" placeholder="Password@123" value={password} onChange={handlePasswordChange} required />
+            <div className="input-container">
+              <label htmlFor="password">Password</label>
+              <input type="password" placeholder="Password@123" name="password" value={password} onChange={handlePasswordChange} required />
             </div>
-            <button class="dark-btn" type="submit" disabled={!isLoginFormValid()}>Login</button>
+            <button className="dark-btn" type="submit" disabled={!isLoginFormValid()}>Login</button>
             <p><a href="/register">Don't have an account? Sign up!</a></p>
           </form>
         </div>
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import EmailValidation from '../models/EmailValidation.js';
 import PasswordValidation from '../models/PasswordValidation.js';
 import NameValidation from '../models/NameValidation.js';
 import { isEmailValid, isValidPassword } from '../utils/register.utils.js';
+import { useNavigate } from 'react-router-dom';
 
-export default function Register() {
+export default function Register({ isValid }) {
+  
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (isValid) {
+      navigate("/dashboard");
+      return;
+    }
+  }, [isValid, navigate]);
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -28,28 +39,63 @@ export default function Register() {
     return isEmailValid(email) && isValidPassword(password);
   };
 
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    if (!isRegisterFormValid()) {
+      console.error('Invalid form data');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data.message === 'Register successful') {
+          window.location.href = '/dashboard';
+        }
+        else{
+          alert(data.message);
+        }
+      } else {
+        const errorData = await response.json();
+        console.error(errorData.message);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div>
-      <div class="center-content">
-        <div class="card">
+      <div className="center-content">
+        <div className="card">
           <h1>Create an Account</h1>
-          <form class="form-area" action="/login" method="POST">
-            <div class="input-container">
-              <label for="name">Name</label>
-              <input type="text" placeholder="PrathamJaiswal" value={name} onChange={handleNameChange} onFocus={() => setNameInputFocused(true)} />
+          <form className="form-area" onSubmit={handleRegister}>
+            <div className="input-container">
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder="PrathamJaiswal" name="name" value={name} onChange={handleNameChange} onFocus={() => setNameInputFocused(true)} />
               { nameInputFocused && <NameValidation name={name} /> }
             </div>  
-            <div class="input-container">
-              <label for="email">Email</label>
-              <input type="text" placeholder="email@example.com" value={email} onChange={handleEmailChange} onFocus={() => setEmailInputFocused(true)} />
+            <div className="input-container">
+              <label htmlFor="email">Email</label>
+              <input type="text" placeholder="email@example.com" name="email" value={email} onChange={handleEmailChange} onFocus={() => setEmailInputFocused(true)} />
               { emailInputFocused && <EmailValidation email={email} /> }
             </div>
-            <div class="input-container">
-              <label for="password">Password</label>
-              <input type="password" placeholder="Password@123" value={password} onChange={handlePasswordChange} onFocus={() => setPasswordInputFocused(true)} />
+            <div className="input-container">
+              <label htmlFor="password">Password</label>
+              <input type="password" placeholder="Password@123" name="password" value={password} onChange={handlePasswordChange} onFocus={() => setPasswordInputFocused(true)} />
               { passwordInputFocused && <PasswordValidation password={password} /> }
             </div>
-            <button class="dark-btn" type="submit" disabled={!isRegisterFormValid()}>Login</button>
+            <button className="dark-btn" type="submit" disabled={!isRegisterFormValid()}>Login</button>
             <p><a href="/login">Already have an account? Sign in!</a></p>
           </form>
         </div>
